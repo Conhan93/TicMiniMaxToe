5fa19c2f23d8f387fa048442af4710fcd7298bd9
Conny Hansson <conhan@hotmail.se>
2021-01-11 16:06:00 +0100

    moved display board

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -14,22 +14,23 @@ TicTacToe::TicTacToe(int size)
 }
 void TicTacToe::start()
 {
+	UI.display_board();
 	while (!engine.is_tie() && !engine.check_win(static_cast<PlayerType>(engine.get_turn())))
 	{
 		Move move;
 		if (engine.get_turn() % 2)
 		{
-			UI.display_board();
+			
 			UI.make_move();
 		}
 		else
 		{
 			move = AI.get_move();
 			engine.make_move(move.row, move.col);
-			UI.display_board();
+			
 		}
 
-		
+		UI.display_board();
 		engine.next_turn();
 	}
 	std::cout << "game ova";
